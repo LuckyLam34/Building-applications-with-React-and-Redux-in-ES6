fa05ad7eac70f47eb62a257f29864af204f3d112
Lucky <lucky-lam@dealercell.com.au>
2019-01-22 17:34:21 +0700

    first commit

---
+++ src/components/about/about-page.js
@@ -0,0 +1,14 @@
+import {React} from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>About Lucky</h1>
+                <p>Lucky is fantastic, that's the truth!</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ tools/startMessage.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+
+console.log('It\'s already started babe');
